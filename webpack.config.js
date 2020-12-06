4255877408c69c5e3d1ad9ed70afba44a56589b0
weianofsteel <skywalker0631@gmail.com>
2020-12-06 23:51:51 +0800

    edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,71 @@
+// const path = require('path');
+
+// module.exports = {
+//     entry: './index.js',
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: 'bundle.js',
+//         libraryTarget: 'commonjs2',
+//     },
+//     resolve: {
+//         extensions: ['.js', '.jsx']
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader',
+//                 exclude: /node_modules/
+//             }
+//         ]
+//     }
+// }
+
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        libraryTarget: 'commonjs2',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
+  mode: 'production',
+  entry: './src/CustomizeCarousel.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: 'CustomizeCarousel.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     }
-}
+  },
+  externals: {
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
