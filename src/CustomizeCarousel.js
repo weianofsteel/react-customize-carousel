ae97abea67a8030fca306e2631db0904fe20a75d
Weian Wang <weian.wang@wakeasp.com.tw>
2020-12-07 18:53:49 +0800

    edit webpack.config.js

--- src/CustomizeCarousel.js
+++ src/CustomizeCarousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
@@ -14,28 +14,168 @@ class CustomizeCarousel extends React.Component {
             count: 0
         }
 
+        this.handleTimer = this.handleTimer.bind(this);
         this.timer = this.timer.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+        this.handleBack = this.handleBack.bind(this);
     }
 
     componentDidMount() {
-
+        this.handleTimer();
     }
 
     componentWillUnmount() {
+        clearInterval(this.handleTimer);
+    }
 
+    handleTimer() {
+        setInterval(()=>{this.timer();},1000);
     }
 
     timer() {
-        this.setState({count: count + 1});
+        this.setState({count: this.state.count + 1});
+    }
+
+    handleNext() {
+        if(!this.props.rotateBy) {
+            this.setState({count: this.state.count + 3})
+        }else {
+            this.setState({count: this.state.count + this.props.rotateBy})
+        }
+    }
+
+    handleBack() {
+        if(!this.props.rotateBy) {
+            if(this.state.count > 3) {
+                this.setState({count: this.state.count - 3});
+            }else {
+                this.setState({count: 0});
+            }
+        }else {
+            if(this.state.count > 3) {
+                this.setState({count: this.state.count - this.props.rotateBy})
+            }else {
+                this.setState({count: 0});
+            }
+            
+        }
     }
 
     render() {
+        
+        const {
+            imgsrc,
+            rotateBy,
+            width,
+            height,
+            controlButton,
+            navigateButton
+        } = this.props;
+
+        const list = [];
+
+        if(!imgsrc) {
+            return list
+        }else {
+            if(!rotateBy) {
+                for(let i = 0; i <= imgsrc.length - 1; i++) {
+                    list.push(
+                        <React.Fragment>
+                            {parseInt( this.state.count / 3) % imgsrc.length === i ?
+                                <div style={{width:width?width:'600px', height:height?height:'500px'}}>
+                                    <img src={imgsrc[i]} style={{margin: '0 auto', width:'100%', height:'100%'}}/>
+                                </div>
+                            :''}        
+                        </React.Fragment>
+                    )
+                }
+            }else {
+                for(let i = 0; i <= imgsrc.length - 1; i++) {
+                    list.push(
+                        <React.Fragment>
+                            {parseInt( this.state.count / rotateBy) % imgsrc.length === i ?
+                                <div style={{width:width?width:'600px', height:height?height:'500px'}}>
+                                    <img src={imgsrc[i]} style={{margin: '0 auto', width:'100%', height:'100%'}}/>
+                                </div>
+                            :''}        
+                        </React.Fragment>
+                    )
+                }
+            }
+        }
+    
+        const RadioNavigation = [];
+
+        if(!imgsrc) {
+            return RadioNavigation
+        }else {
+            if(!rotateBy) {
+                for(let i = 0; i <= imgsrc.length - 1; i++) {
+                    RadioNavigation.push(
+                        <React.Fragment>
+                            {parseInt( this.state.count / 3) % imgsrc.length === i ?
+                                <IconButton onClick={()=>{this.setState({count: 3 * i})}}>
+                                    <PlayCircleFilledIcon/>
+                                </IconButton>
+                            :
+                                <IconButton onClick={()=>{this.setState({count: 3 * i})}}>
+                                    <RadioButtonUncheckedIcon/>
+                                </IconButton>
+                            }
+                        </React.Fragment>
+                    )
+                }
+            }else {
+                for(let i = 0; i <= imgsrc.length - 1; i++) {
+                    RadioNavigation.push(
+                        <React.Fragment>
+                            {parseInt( this.state.count / rotateBy) % imgsrc.length === i ?
+                                <IconButton onClick={()=>{this.setState({count: rotateBy * i})}}>
+                                    <PlayCircleFilledIcon/>
+                                </IconButton>
+                            :
+                                <IconButton onClick={()=>{this.setState({count: rotateBy * i})}}>
+                                    <RadioButtonUncheckedIcon/>
+                                </IconButton>
+                            }
+                        </React.Fragment>
+                    )
+                }
+            }
+        }
+
         return(
-            <React.FragmentF>
+            <React.Fragment>
 
-                <div></div>
+                <div>{this.state.count}</div>
 
-            </React.FragmentF>
+                <Grid container justify="center" alignItems="center">
+                
+                    {controlButton === true ?
+                        <Button style={{margin:'auto 0'}} onClick={this.handleBack}>
+                            <ArrowBackIosIcon/>
+                        </Button>
+                    :''}
+
+                    {list}
+
+                    {controlButton === true ?
+                        <Button style={{margin:'auto 0'}} onClick={this.handleNext}>
+                            <ArrowForwardIosIcon/>
+                        </Button>
+                    :''}
+                
+                </Grid>
+
+                {navigateButton === true ?
+                    <Grid container justify="center" alignItems="center">
+                    
+                        {RadioNavigation}
+                    
+                    </Grid>
+                :''}
+
+            </React.Fragment>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//     entry: './index.js',
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'bundle.js',
-//         libraryTarget: 'commonjs2',
-//     },
-//     resolve: {
-//         extensions: ['.js', '.jsx']
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/,
-//                 loader: 'babel-loader',
-//                 exclude: /node_modules/
-//             }
-//         ]
-//     }
-// }
-
 const path = require('path');
 
 module.exports = {
